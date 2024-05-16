201ab27d7c341e4a537eaf94cf554d976bdc5eb2
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-05-16 23:11:44 +0900

    [BOJ] 11382

---
+++ Algorithm/BOJ/11382.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    
+    long long a,b,c;
+    cin>>a>>b>>c;
+    cout<<a+b+c<<endl;
+    
+}
+//간당한 입출력 문제이지만 10^12 x 3일 경우 int 자료형의 범위를 초과하기 때문에 int 보다 더 큰 범위인 long long으로 변수를 선언하다.
