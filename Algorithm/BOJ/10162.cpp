c4a3e2096fcf94040a4f1417d73cc8c5cabc1e3f
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-05-18 19:52:48 +0900

    [BOJ] 10162
    Bronze Greedy

---
+++ Algorithm/BOJ/10162.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  ex
+//
+//  Created by 김상준 on 5/18/24.
+//
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    // A : 300초, B : 60초, C : 10초
+    int a, b, c = 0;
+    int t;
+    cin >> t;
+
+    if (t % 10 != 0) cout << "-1";
+    else {
+        a = t / 300;
+        b = (t % 300) / 60;
+        c = ((t % 300) % 60) / 10;
+        cout << a << " " << b << " " << c;
+    }
+
+    return 0;
+
+}
