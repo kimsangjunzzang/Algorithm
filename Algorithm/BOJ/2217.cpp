966b801e4e0675b5823c25b44df975ce2eb2da05
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-06-02 21:11:32 +0900

    [BOJ]Silver Greedy

---
+++ Algorithm/BOJ/2217.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  as
+//
+//  Created by 김상준 on 6/2/24.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+ 
+using namespace std;
+ 
+int rope[100001];
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        cin >> rope[i];
+    sort(rope, rope + N, greater<int>());
+ 
+    long long result = 0;
+    for (int i = 0; i < N; i++) {
+        long long sum = rope[i] * (i + 1);
+        if(sum > result)
+            result = sum;
+    }
+    cout << result;
+ 
+    return 0;
+}
