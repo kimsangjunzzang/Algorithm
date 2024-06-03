3bb5d9356cc24714fd2b62dc95816891d58bb19a
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-06-03 23:59:08 +0900

    [BOJ]Silver 구현

---
+++ Algorithm/BOJ/1748.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    int cnt = 0;
+
+    cin >> n;
+
+    if (n < 10) {
+        cout << n;
+        return 0;
+    }
+
+    for (int i = 1; i <= n; i *= 10) {
+        cnt += n - i + 1;
+    }
+
+    cout << cnt;
+
+    return 0;
+}
