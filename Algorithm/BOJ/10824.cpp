9f6e017a571e7c9b4b8aee0f2cd01be313ceb9a9
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-06-04 00:26:18 +0900

    [BOJ]Bronze String

---
+++ Algorithm/BOJ/10824.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string a,b,c,d;
+    cin>>a>>b>>c>>d;
+    
+    string sum1 = a+b;
+    string sum2 = c+d;
+    
+    cout<<stoll(sum1)+stoll(sum2)<<endl;
+    
+    
+}
