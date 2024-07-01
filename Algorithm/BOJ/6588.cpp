e33881de7305521672fada41f70741d2b149d436
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-07-02 08:28:17 +0900

    [BOJ] Silver 에라토스테네스의 체
    주석 처리

--- Algorithm/BOJ/6588.cpp
+++ Algorithm/BOJ/6588.cpp
@@ -2,8 +2,9 @@
 #include <vector>
 using namespace std;
 
-vector<bool> prime(1000001,true);
+vector<bool> prime(1000001,true); // 소수 전체 배열 true 초기화
 
+// 소수가 아닌 숫자 false로 전환
 void PrimeCheck() {
     for (long long i = 2; i * i <= 1000000; i++) {
         for (long long j = i * i; j <= 1000000; j += i) {
@@ -18,13 +19,14 @@ int main() {
     ios_base::sync_with_stdio(false);
     
     long long n;
-    PrimeCheck();
+    PrimeCheck(); // 함수 실행으로 배열 셋업
     
     while (cin>>n) {
         
-        if (n == 0) {
+        if (n == 0 || n == 1) {
             break;
         }
+        // prime 배열 중 주어진 수보다 작은 수 중에 가장 큰 수와 가장 작은 수 구함.
         
         for (long long i = 2; i <= n; i++) {
             if ((prime[i] == true) && (prime[n - i] == true)) {
@@ -32,5 +34,6 @@ int main() {
                 break;
             }
         }
+        
     }
 }
