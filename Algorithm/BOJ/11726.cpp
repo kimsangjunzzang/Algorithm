3392d2c41aff674dd1c157a3e3e9b35e310e135f
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-07-04 08:45:39 +0900

    [BOJ] Silver DP
    [BOJ] Silver DP

---
+++ Algorithm/BOJ/11726.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  알고리즘
+//
+//  Created by 김상준 on 7/4/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int dp[1001];
+    dp[0]=0;
+    dp[1]=1;
+    dp[2]=2;
+    for(int i=3;i<1001;i++){
+        dp[i] = (dp[i-1] + dp[i-2])%10007;
+    }
+    
+    int num;
+    cin>>num;
+    cout<<dp[num]<<endl;
+    
+}
