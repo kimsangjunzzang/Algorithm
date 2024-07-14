2a04219434cfa9efed490299882c0b64b05c36d0
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-07-14 11:57:41 +0900

    [BOJ] Silver DP
    [BOJ] Silver DP

---
+++ Algorithm/BOJ/9655.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  알고리즘
+//
+//  Created by 김상준 on 7/4/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin>> num;
+    
+    if(num % 2 ==0){
+        cout<<"CY"<<endl;
+    }
+    else {
+        cout<<"SK"<<endl;
+    }
+    
+}
