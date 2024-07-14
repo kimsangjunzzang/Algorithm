be86ee6a063de84fbaa2fd58e3bc22c62bd67412
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-07-14 14:19:59 +0900

    [BOJ] Silver Map
    [BOJ] Silver Map

---
+++ Algorithm/BOJ/14425.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  알고리즘
+//
+//  Created by 김상준 on 7/4/24.
+//
+
+#include <iostream>
+#include <set>
+#include <string>
+using namespace std;
+
+int N, M, ans;
+string n_str, m_str;
+set<string> n;
+
+int main() {
+    cin >> N >> M;
+    
+    for (int i = 0; i < N; i++) {
+        cin >> n_str;
+        n.insert(n_str);
+    }
+    for (int i = 0; i < M; i++) {
+        cin >> m_str;
+        if (n.find(m_str) != n.end()) {
+            ans++;
+        }
+    }
+    cout << ans;
+    
+    return 0;
+}
