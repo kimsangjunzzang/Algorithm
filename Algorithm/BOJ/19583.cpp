467f7fdb71c70fbaefc283919d601362d32028b7
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-07-16 08:57:21 +0900

    [BOJ] Silver Set
    [BOJ] Silver Set

---
+++ Algorithm/BOJ/19583.cpp
@@ -0,0 +1,54 @@
+//
+//  main.cpp
+//  알고리즘
+//
+//  Created by 김상준 on 7/4/24.
+//
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int STT(string s) {
+    int h = stoi(s.substr(0, 2));
+    int m = stoi(s.substr(3, 2));
+    int time = h * 100 + m;
+    return time;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    string s, e, q;
+    cin >> s >> e >> q;
+    
+    int s_time = STT(s);
+    int e_time = STT(e);
+    int q_time = STT(q);
+
+    set<string> us;
+    set<string> uscnt;
+
+    while (1) {
+        string s1, s2;
+        cin >> s1 >> s2;
+        
+        if (s1 == "" && s2 == "") {
+            break;
+        }
+        
+        int s1_time = STT(s1);
+
+        if (s1_time <= s_time) {
+            us.insert(s2);
+        }
+        
+        else if (s1_time >= e_time && s1_time <= q_time) {
+            if(us.find(s2)!=us.end())
+            uscnt.insert(s2);
+        }
+    }
+    cout << uscnt.size() << '\n';
+}
