d4df746384b1e91e2b6a1da3378782528661f070
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-08-13 01:07:39 +0900

    [BOJ] Bronze 구현
    [BOJ] Bronze 구현

---
+++ Algorithm/BOJ/3003.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  알고리즘
+//
+//  Created by 김상준 on 8/7/24.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d,e,f;
+    cin >> a >> b >> c >> d >> e >> f;
+    
+    cout << 1-a <<" "<<1-b<<" "<<2-c<<" "<<2-d<<" "<<2-e<<" "<<8-f<<endl;
+    
+}
