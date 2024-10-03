b5fdd718863a1fc1a6772c2358dc45d56f0f385c
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-04 00:06:45 +0900

    [Programmers] Lv0 문자열 출력하기
    코딩 기초 트레이닝

---
+++ Algorithm/프로그래머스/Lv.0/문자열 출력하기.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  0519
+//
+//  Created by 김상준 on 5/19/24.
+//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    cout << str;
+    return 0;
+}
