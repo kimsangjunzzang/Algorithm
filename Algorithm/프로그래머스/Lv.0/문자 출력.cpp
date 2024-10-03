89eaaeb34059a45b635865b112ae67aad3ffa790
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-03 21:14:40 +0900

    [Programmers] Lv0 문자 출력
    [PCCE 기출문제] 1번 / 문자 출력

---
+++ Algorithm/프로그래머스/Lv.0/문자 출력.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  0519
+//
+//  Created by 김상준 on 5/19/24.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    string message = "Let's go!";
+
+    cout << "3\n2\n1" << endl;
+    cout << message << endl;
+
+    return 0;
+}
