275e7a55ed07498fa0ce79e00566bbb199d090ff
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-15 23:34:02 +0900

    [BOJ 1292/C++] 쉽게 푸는 문제
    브론즈

---
+++ Algorithm/BOJ/1292.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  알고리즘
+//
+//  Created by 김상준 on 8/7/24.
+//
+#include <iostream>
+
+using namespace std;
+int main(){
+    int arr[1001],n,m,ans=0,k=1;
+    for(int i=1; i<=1000; i++){
+        for(int j=1; j<=i; j++){
+            arr[k]=i;
+            if(k>1000){
+                break;
+            }
+            k++;
+        }
+    }
+    cin>>n>>m;
+    for(int i=n; i<=m; i++){
+        ans+=arr[i];
+    }
+    cout<<ans<<endl;
+}
