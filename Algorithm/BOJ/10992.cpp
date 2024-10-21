1aa6c301b91d6ceade5b786e6135d1774bfbcfe3
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-21 23:14:43 +0900

    [BOJ 10992/C++] 별 찍기 - 17
    브론즈

---
+++ Algorithm/BOJ/10992.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  알고리즘
+//
+//  Created by 김상준 on 7/4/24.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num; cin >> num;
+    for (int i = 1; i <= num; i++) {
+        if (i == num) {    for (int i = 1; i <= 2*num-1; i++) cout << "*";
+        } else {
+            for (int j = num - i; j > 0; j--)  cout << " " ;
+            cout << "*";
+            if (i != 1) {
+                for (int j = 1; j <= (i - 1) * 2 - 1; j++) { cout << " "; }
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
