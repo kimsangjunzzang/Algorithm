9e675dfdbd3e6e04dc559e9d356ca5b4fb28acfd
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-22 19:21:47 +0900

    [BOJ C++/4999] 아!
    브론즈

---
+++ Algorithm/BOJ/4999.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  알고리즘
+//
+//  Created by 김상준 on 8/7/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str1, str2; cin >> str1 >> str2;
+
+    if(str1.length() >= str2.length()) {
+        cout << "go"<<endl;
+    } else {
+        cout << "no"<<endl;
+    }
+
+}
