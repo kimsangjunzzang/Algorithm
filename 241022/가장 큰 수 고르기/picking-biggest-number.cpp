80553c6b4d702601f30f916f9dd137cad25a0b06
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-22 20:35:37 +0900

    Title: 가장 큰 수 고르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241022/가장 큰 수 고르기/picking-biggest-number.cpp
+++ 241022/가장 큰 수 고르기/picking-biggest-number.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    max = 0;
+    int max = 0;
     for ( int i=0;i<10;i++) {
         int num; cin >> num;
         if(num > max) max = num; 
