ec0ebee2ad5becf11add873fa75aa1525ba81aaf
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-22 20:35:27 +0900

    Title: 가장 큰 수 고르기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241022/가장 큰 수 고르기/picking-biggest-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    max = 0;
+    for ( int i=0;i<10;i++) {
+        int num; cin >> num;
+        if(num > max) max = num; 
+    }
+    cout << max;
+    return 0;
+}
