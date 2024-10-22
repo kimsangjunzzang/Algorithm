8cbb2d916bdf98a90e90e37b1baa950a7ae0846e
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-22 20:06:39 +0900

    Title: 십의 자리 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241022/십의 자리 숫자/number-of-tens-digit.cpp
+++ 241022/십의 자리 숫자/number-of-tens-digit.cpp
@@ -5,23 +5,31 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    vector<int>v(10);
+    vector<int> v(10);  // 0~9까지의 카운트를 저장할 벡터
 
-    for ( int i=0;i<10;i++) {
-        string str; cin >> str;
-        int num;
+    while (true) {
+        string str;
+        cin >> str;
 
-        if(str.length() == 2) {
-            num = str[0]-48;
-            v[num]++;
-        } else {
-            num = 0;
-            v[num]++;
+        // "0"이 입력되면 반복을 종료
+        if (str == "0") {
+            break;
         }
+
+        int num;
+        if (str.length() == 2) {  // 길이가 2인 경우 첫 번째 문자를 숫자로 변환
+            num = str[0] - '0';   // '0'의 아스키 값을 빼서 숫자로 변환
+        } else {                  // 그 외의 경우 0으로 처리
+            num = 0;
+        }
+
+        // 입력된 숫자의 카운트를 증가
+        v[num]++;
     }
-    for ( int i=1;i<10;i++) {
-        cout << i <<" - "<<v[i]<<endl;
+
+    // 1~9까지의 숫자 카운트 출력
+    for (int i = 1; i < 10; i++) {
+        cout << i << " - " << v[i] << endl;
     }
 
     return 0;
