1301e68e179e0c6fed53befe2f351870d6c7877a
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-31 00:38:53 +0900

    [BOJ 5338/C++] 마이크로소프트 로고
    브론즈

---
+++ Algorithm/BOJ/5338.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  알고리즘
+//
+//  Created by 김상준 on 8/7/24.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "       _.-;;-._" << endl;
+    cout << "'-..-'|   ||   |" << endl;
+    cout << "'-..-'|_.-;;-._|" << endl;
+    cout << "'-..-'|   ||   |" << endl;
+    cout << "'-..-'|_.-''-._|" << endl;
+
+    return 0;
+}
