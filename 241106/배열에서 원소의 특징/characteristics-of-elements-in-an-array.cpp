e59b462b73105c6f0c0c01985f992a0c27ca4740
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-11-06 00:30:42 +0900

    Title: 배열에서 원소의 특징, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241106/배열에서 원소의 특징/characteristics-of-elements-in-an-array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    vector<int> v(10);
+
+    for ( int i=0;i<10;i++) {
+        cin >> v[i];
+    }
+    for ( int i=0;i<v.size();i++){
+        if(v[i]%3==0) {
+            cout<<v[i-1]<<endl;
+        break;
+        }
+    }
+    return 0;
+}
