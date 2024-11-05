9c5310675de6a644c3eff34c84d19b95e2887712
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-11-06 00:48:27 +0900

    Title: 특정 원소들의 합, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241106/특정 원소들의 합/sum-of-specific-elements.cpp
+++ 241106/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -6,13 +6,19 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int arr [4][4];
     int cnt =0;
+
     for ( int i=0; i<4; i++) {
-        for ( int j =0; j<4;i++) {
+        for ( int j =0; j<4;j++) {
             int num; cin >> num;
             arr[i][j] = num;
         }
     }
-    cnt = arr[0][0] + arr[1][0] + arr[1][1] + arr[2][0] + arr[2][1] + arr[2][2] +arr[3][0] + +arr[3][1] + +arr[3][2] + +arr[3][3];
-    cout << cnt <<endl;
+
+    for ( int i =0;i<4;i++) {
+        for ( int j=0;j<4;j++) {
+            if(j<=i)cnt +=arr[i][j];
+        }
+    }
+    cout << cnt;
     return 0;
 }
