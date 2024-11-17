81cde8f1325ef60a94c749577610420fd70cddb3
kimsangjunzzang <lmk0347@naver.com>
2024-11-17 17:05:34 +0900

    Title: 지그재그로 숫자 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241117/지그재그로 숫자 채우기/zigzag-numbering.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언:
+    int n, m;
+    int answer[100][100] = {0};
+
+    // 입력:
+    cin >> n >> m;
+    
+    // Step 1:
+    int count = 0;
+    for(int col = 0; col < m; col++) {
+        if (col % 2 == 0){
+            // Case 1:
+            for(int row = 0; row < n; row++){
+                answer[row][col] = count;
+                count++;
+            }
+        }
+        else {
+            // Case 2:
+            for(int row = n - 1; row >= 0; row--){
+                answer[row][col] = count;
+                count++;
+            }
+        }
+    }
+    
+    // 출력:
+    for(int row = 0; row < n; row++) {
+        for(int col = 0; col < m; col++) 
+            cout << answer[row][col] << " ";
+        cout << endl;
+    }
+    return 0;
+}
