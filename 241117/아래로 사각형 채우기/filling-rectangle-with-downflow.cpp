d471b915aae03c6087cb1be4d677f937cc757a31
kimsangjunzzang <lmk0347@naver.com>
2024-11-17 16:57:19 +0900

    Title: 아래로 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241117/아래로 사각형 채우기/filling-rectangle-with-downflow.cpp
+++ 241117/아래로 사각형 채우기/filling-rectangle-with-downflow.cpp
@@ -7,7 +7,7 @@ int main() {
     int arr[num][num]={};
 
     for ( int i=0;i<num;i++) {
-        if(i=0) arr[i][0] = 1;
+        if(i==0) arr[i][0] = 1;
         else {
         arr[i][0]= arr[i-1][0] +1;
         }
