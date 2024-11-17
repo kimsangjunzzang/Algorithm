0f70add2861002fce23c7964d0965d967ad5776b
kimsangjunzzang <lmk0347@naver.com>
2024-11-17 17:10:09 +0900

    Title: 반복 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241117/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void printFunc(int num){
+    for( int i=0;i<num;i++) {
+        cout << "12345^&*()_"<<endl;
+    }
+
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num; cin >> num;
+    printFunc(num);
+    return 0;
+}
