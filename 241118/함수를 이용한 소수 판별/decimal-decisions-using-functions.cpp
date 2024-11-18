37c95f207626a06893055cd7a871239b87d6b8bd
kimsangjunzzang <lmk0347@naver.com>
2024-11-18 22:51:29 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241118/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 241118/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -1,20 +1,13 @@
-//
-//  main.cpp
-//  알고리즘
-//
-//  Created by 김상준 on 8/7/24.
-//
-
 #include <iostream>
 using namespace std;
 
 bool IsPrime(int n) {
     if(n == 1) return false;
-    else {
+    
         for(int i = 2; i < n; i++) {
             if(n % i == 0) return false;
         }
-    }
+
     return true;
 }
 
