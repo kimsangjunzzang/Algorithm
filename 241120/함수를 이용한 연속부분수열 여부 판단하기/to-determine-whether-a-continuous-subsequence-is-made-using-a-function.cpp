b7a6eff9558c3684ce22281a9183e431c978ce71
kimsangjunzzang <lmk0347@naver.com>
2024-11-20 07:19:11 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241120/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 241120/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
+// 수열 B가 수열 A의 연속 부분 수열인지 확인하는 함수
+bool isSubsequence(const vector<int>& A, const vector<int>& B) {
+    int n1 = A.size(), n2 = B.size();
+    for (int i = 0; i <= n1 - n2; ++i) { // 가능한 시작 위치 탐색
+        bool match = true;
+        for (int j = 0; j < n2; ++j) { // 연속 부분 비교
+            if (A[i + j] != B[j]) {
+                match = false;
+                break;
+            }
+        }
+        if (match) return true; // 일치하는 경우
+    }
+    return false; // 끝까지 일치하지 않으면 false
+}
+
 int main() {
-    // 여기에 코드를 작성해주세요.
-    int a,b; cin >> a >> b;
-    string str1;
-    string str2;
-
-    for( int i=0;i<a;i++) {
-        char c; cin >> c;
-        str1 += c;
+    int n1, n2;
+    cin >> n1 >> n2;
+    
+    vector<int> A(n1), B(n2);
+    
+    // 수열 A 입력
+    for (int i = 0; i < n1; ++i) {
+        cin >> A[i];
     }
-
-    for ( int i=0;i<b;i++) {
-        char c; cin >> c;
-        str2 += c;
+    
+    // 수열 B 입력
+    for (int i = 0; i < n2; ++i) {
+        cin >> B[i];
     }
-
-    if(str1.find(str2) != string::npos) cout << "Yes" << endl;
-    else cout << "No" << endl;
-
+    
+    // 결과 출력
+    if (isSubsequence(A, B)) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    
     return 0;
 }
