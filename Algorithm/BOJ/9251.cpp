3af8380156ccecaf85eb08fd04e91cf4046ed8ad
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-11-28 08:39:28 +0900

    [BOJ 9251/C++] LCS
    수정

--- Algorithm/BOJ/9251.cpp
+++ Algorithm/BOJ/9251.cpp
@@ -15,15 +15,15 @@ int main() {
     cin.tie(0); cout.tie(0);
 
     int dp[1001][1001]={0};
-    string a, b; cin >> a >> b;
+    string str1, str2; cin >> str1 >> str2;
 
-    for (int i = 1; i <= a.length(); i++) {
-        for (int j = 1; j <= b.length(); j++) {
+    for (int i = 1; i < str1.length()+1; i++) {
+        for (int j = 1; j < str2.length()+1; j++) {
 
-            if (a[i - 1] == b[j-1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+            if (str1[i-1] == str2[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
 
-            else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
         }
     }
-    cout << dp[a.length()][b.length()]<<endl;
+    cout << dp[str1.length()][str2.length()]<<endl;
 }
